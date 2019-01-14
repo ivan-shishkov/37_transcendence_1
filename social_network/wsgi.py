b90e2bf5223c6e70b619bb4aa0ec33c6f770ab57
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-01-14 23:16:20 +0300

    Moved import of configuration for correct work with uWSGI

--- social_network/wsgi.py
+++ social_network/wsgi.py
@@ -1,11 +1,12 @@
 import os
 
-from configurations.wsgi import get_wsgi_application
 import dotenv
 
 dotenv.read_dotenv(
     os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'),
 )
+
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 
