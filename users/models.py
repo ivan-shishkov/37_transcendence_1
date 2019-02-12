80706ea8737b4f71faf4b4001a01949c7fde3645
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-02-12 21:11:57 +0300

    Marked CustomUser fields 'first_name', 'last_name' and 'email' as required

--- users/models.py
+++ users/models.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 
 
 class CustomUser(AbstractUser):
+    first_name = models.CharField(
+        'first name', max_length=30, help_text='Required')
+    last_name = models.CharField(
+        'last name', max_length=150, help_text='Required')
+    email = models.EmailField('email address', help_text='Required')
+
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
