a94060c71aa0670320353cf512b50eb855dce21d
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-02-16 17:11:49 +0300

    Added filter of users by having avatar

--- users/admin.py
+++ users/admin.py
@@ -4,8 +4,27 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 
+class HavingAvatarListFilter(admin.SimpleListFilter):
+    title = 'having avatar'
+    parameter_name = 'having_avatar'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'have avatar'),
+            ('no', 'no avatar'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.exclude(avatar='')
+
+        if self.value() == 'no':
+            return queryset.filter(avatar='')
+
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+    list_filter = (HavingAvatarListFilter,)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
