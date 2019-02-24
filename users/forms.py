e4571f83352e415840eb74a5a52ea5a8cceda718
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-02-24 09:40:11 +0300

    Changed password field for display validation errors for better usability

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ValidationError
+from django.contrib.auth import password_validation
 from captcha.fields import CaptchaField
 
 from .models import CustomUser
@@ -10,3 +12,14 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'first_name', 'last_name',)
+
+    def _post_clean(self):
+        super(UserCreationForm, self)._post_clean()
+
+        password = self.cleaned_data.get('password1')
+
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error('password1', error)
